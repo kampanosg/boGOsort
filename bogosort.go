87cb1bab89dbefb5781652a93ab9cfa10a45b471
George Kampanos <george.kampanos@nandos.co.uk>
2023-07-14 15:40:04 +0100

    added skeleton

---
+++ bogosort.go
@@ -0,0 +1,16 @@
+package bogosort
+
+func Sort(s []any) []any {
+	if len(s) < 2 {
+		return s
+	}
+	return nil
+}
+
+func shuffle(s []any) []any {
+	return nil
+}
+
+func isSorted(s []any) bool {
+	return false
+}
